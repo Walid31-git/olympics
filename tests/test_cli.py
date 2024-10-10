d3a2bfa9e76a207e968fedb754cfe07da3ddfee5
Ghouti <ghoutiallal@gmail.com>
2024-10-10 14:33:43 +0200

    tests pour cli.py et correction du bug caché et correction de config

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,3 +8,21 @@ def test_top_countries():
     cli.top_countries(file=string)
     text = string.getvalue()
     assert 'Top' in text
+
+def test_top_athletes():
+    string = StringIO()
+    cli.top_athletes(file=string)
+    text = string.getvalue()
+    assert 'Top' in text
+
+def test_top_disciplines():
+    string = StringIO()
+    cli.top_disciplines(file=string)
+    text = string.getvalue()
+    assert 'Top' in text
+
+def test_top_teams():
+    string = StringIO()
+    cli.top_teams(file=string)
+    text = string.getvalue()
+    assert 'Top' in text
