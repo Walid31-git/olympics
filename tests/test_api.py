5a134d65fabdae544b320da3f78362775be0f36d
Ghouti <ghoutiallal@gmail.com>
2024-10-10 13:11:36 +0200

    ajout de tests dans test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -10,3 +10,18 @@ def test_countries():
     response = client.get('/countries/')
     assert response.status_code == 200
     assert len(response.json()) > 100
+
+def test_disciplines():
+    response = client.get('/disciplines/')
+    assert response.status_code == 200
+    assert len(response.json()) > 40
+    
+def test_teams():
+    response = client.get('/teams/')
+    assert response.status_code == 200
+    assert len(response.json()) > 100
+    
+def test_medals():
+    response = client.get('/medals/')
+    assert response.status_code == 200
+    assert len(response.json()) > 100
